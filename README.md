444d41117cd1622da910522f5656dc1ce86c250e
tknr <info@tknr.com>
2018-11-16 02:18:33 +0000

    2018年 11月 16日 金曜日 02:18:33 UTC

--- README.md
+++ README.md
@@ -2,12 +2,9 @@
 
 https://www.centos.org/forums/viewtopic.php?f=47&t=68050&sid=e1a7120bbfa8117a4e8d2dbf7ed5a0b7&start=10
 
-execute
-./install_cheat.sh
+## install
+$ ./install_cheat.sh
 
-and
-
-add this line in dropbox.py, right before subprocess.Popen....., in function start_dropbox
-
-os.environ['LD_PRELOAD'] = "/usr/local/lib64/drobpoxcheat.so"
+## execute
+$ dropbox_daemon
 
--- install_cheat.sh
+++ install_cheat.sh
@@ -4,3 +4,17 @@ gcc -Wall -fPIC -shared -o drobpoxcheat.so drobpoxcheat.c || exit 1
 mkdir -p /usr/local/lib64
 cp drobpoxcheat.so /usr/local/lib64/
 
+mkdir -p ~/bin
+ln -s `pwd`/dropbox_daemon ~/bin/
+
+export_count=`cat ~/.bash_profile | grep 'export PATH="$HOME/bin:$PATH"' | wc -l`
+echo $export_count
+
+if [ $export_count -eq 0 ]; then
+        echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bash_profile
+fi
+
+echo "installed."
+echo "execute to complete installation:"
+echo "source ~/.bash_profile"
+
